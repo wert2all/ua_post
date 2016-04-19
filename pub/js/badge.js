8e6be6c2e2199be6651dbbab5d2d1f87280db565
Sergey Shvets <wert2all@gmail.com>
2016-04-19 12:06:45 +0300

    Fixed #12:  Add $dos as dependency to badge.js (#13)

--- pub/js/badge.js
+++ pub/js/badge.js
@@ -1,10 +1,34 @@
 /**
  * Created by wert2all on 4/19/16.
  */
-define(function () {
+define(["libs/dollardom.min"], function () {
+
+    var dataValue,
+        dataTitle = "",
+        dataBackground = true,
+        _getSelector = function () {
+            return "span.mdl-badge" + ( (dataBackground === false) ? ".mdl-badge--no-background" : "" );
+        };
+
     return {
-        stopAnimation: function () {
-            console.log();
+        setData: function (data) {
+            dataValue = data;
+            return this;
+        },
+        setTitle: function (title) {
+            dataTitle = title;
+            return this;
+        },
+        setBackground: function (back) {
+            dataBackground = (back !== false);
+            return this;
+        },
+        build: function () {
+            var badgeDOM = $dom.create(_getSelector());
+            badgeDOM.appendChild(document.createTextNode(dataTitle));
+            badgeDOM.setAttribute("data-badge", dataValue);
+
+            return badgeDOM;
         }
     };
 });
--- pub/js/main.js
+++ pub/js/main.js
@@ -1,8 +1,11 @@
 /**
  * Created by wert2all on 4/19/16.
  */
-
 requirejs(["libs/material.min"]);
 requirejs(["badge"], function (badge) {
-    console.log(badge);
+    var badgeElement = badge.setTitle("title ")
+        .setData("...")
+        .setBackground(false)
+        .build();
+    console.log(badgeElement);
 });
