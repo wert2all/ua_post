bcc026534898132242e438df700ab4622c0c17a5
Sergey Shvets <sshvets@magento.com>
2016-04-19 17:51:02 +0300

    Fixed #14:  Badge.js must return object with methods startAnimation() ant stopAnimation()

--- pub/js/badge.js
+++ pub/js/badge.js
@@ -2,19 +2,56 @@
  * Created by wert2all on 4/19/16.
  */
 define(["libs/dollardom.min"], function () {
-
-    var dataValue,
-        dataTitle = "",
+    var dataTitle = "",
         dataBackground = true,
         _getSelector = function () {
             return "span.mdl-badge" + ( (dataBackground === false) ? ".mdl-badge--no-background" : "" );
         };
 
+    function Badge(DomElement) {
+        var _loaderCount = 1,
+            _dom = DomElement,
+            _timeInterval,
+
+            _setValue = function (value) {
+                _dom.setAttribute("data-badge", value);
+            },
+            _setLoader = function () {
+                // strRepeat from underscore.string
+                function strRepeat(str, qty) {
+                    if (qty < 1) return '';
+                    var result = '';
+                    while (qty > 0) {
+                        if (qty & 1) result += str;
+                        qty >>= 1, str += str;
+                    }
+                    return result;
+                }
+
+                _setValue(strRepeat(".", _loaderCount));
+                _loaderCount++;
+                if (_loaderCount == 4) {
+                    _loaderCount = 1;
+                }
+            };
+
+        return {
+            stopAnimation: function () {
+                clearInterval(_timeInterval);
+            },
+            startAnimation: function () {
+                _timeInterval = setInterval(_setLoader, 500);
+            },
+            setValue: function (value) {
+                _setValue(value);
+            },
+            getDom: function () {
+                return _dom;
+            }
+        }
+    }
+
     return {
-        setData: function (data) {
-            dataValue = data;
-            return this;
-        },
         setTitle: function (title) {
             dataTitle = title;
             return this;
@@ -26,9 +63,7 @@ define(["libs/dollardom.min"], function () {
         build: function () {
             var badgeDOM = $dom.create(_getSelector());
             badgeDOM.appendChild(document.createTextNode(dataTitle));
-            badgeDOM.setAttribute("data-badge", dataValue);
-
-            return badgeDOM;
+            return new Badge(badgeDOM);
         }
     };
 });
