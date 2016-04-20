67016b83739c4cc3681329a66e320c75b34e8a15
wert2all <wert2all@gmail.com>
2016-04-21 00:34:24 +0300

    Fixed #20: Add links to budges

--- pub/js/badge_holder.js
+++ pub/js/badge_holder.js
@@ -12,10 +12,10 @@ define(["libs/dollardom.min"], function () {
             _viewHolder.appendChild(badge.getDom());
         },
         _addBadge = function (badge) {
-            _listBadges[badge.getHash()] = badge;
-            _appendToViewHolder(badge);
+            var decorator = new BadgeDecorator(badge);
+            _listBadges[decorator.getHash()] = decorator;
+            _appendToViewHolder(decorator);
         };
-
 
     _viewHolder = $dom.get("#tracklist");
 
@@ -23,7 +23,26 @@ define(["libs/dollardom.min"], function () {
         throw new Error('Bad holder ');
     }
     _viewHolder = _viewHolder[0];
-    console.log(_viewHolder);
+    
+    function BadgeDecorator(badge) {
+        var _dom = $dom.create("a");
+        _dom.href = "#";
+        _dom.addEventListener('click', function () {
+            console.log(badge.getHash());
+            return false;
+        });
+
+        _dom.appendChild(badge.getDom());
+
+        return {
+            getHash: function () {
+                return badge.getHash();
+            },
+            getDom: function () {
+                return _dom;
+            }
+        }
+    }
 
     return {
         addBadge: function (badge) {
