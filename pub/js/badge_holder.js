ab89cac1b9e8c112d12d41630249355caa82038b
Sergey Shvets <wert2all@gmail.com>
2016-04-22 17:43:11 +0300

    Fixed #23: Don't use global $dom variable (#24)

--- pub/js/badge_holder.js
+++ pub/js/badge_holder.js
@@ -1,9 +1,10 @@
 /**
  * Created by wert2all on 4/20/16.
  */
-define(["libs/dollardom.min"], function () {
+define(["dollardom"], function (dom) {
 
     var _viewHolder,
+        _dom = dom,
         _listBadges = [],
         _isBadgeExist = function (badge) {
             return ( typeof _listBadges[badge.getHash()] !== "undefined");
@@ -12,20 +13,20 @@ define(["libs/dollardom.min"], function () {
             _viewHolder.appendChild(badge.getDom());
         },
         _addBadge = function (badge) {
-            var decorator = new BadgeDecorator(badge);
+            var decorator = new BadgeDecorator(badge, _dom);
             _listBadges[decorator.getHash()] = decorator;
             _appendToViewHolder(decorator);
         };
 
-    _viewHolder = $dom.get("#tracklist");
+    _viewHolder = _dom.get("#tracklist");
 
     if (_viewHolder.length !== 1) {
         throw new Error('Bad holder ');
     }
     _viewHolder = _viewHolder[0];
-    
-    function BadgeDecorator(badge) {
-        var _dom = $dom.create("a");
+
+    function BadgeDecorator(badge, dom) {
+        var _dom = dom.create("a");
         _dom.href = "#";
         _dom.addEventListener('click', function () {
             console.log(badge.getHash());
