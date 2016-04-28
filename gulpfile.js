282d3598e47d0a08ac601f16aa9a7dbfede6e98f
Sergey Shvets <sshvets@magento.com>
2016-04-28 10:05:45 +0300

    Fixed #28: Testing  gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,36 @@
 var gulp = require('gulp'),
-    minify = require('gulp-minify'),
-    eslint = require('gulp-eslint');
+    del = require('del'),
+    config = {
+        js: ["pub/js/*.js", "pub/js/badge/*.js"],
+        release: {
+            folder: 'dist',
+            file: "all.js"
+        }
+    };
 
-gulp.task('default', ['lint'], function () {
+gulp.task('default', ['clean', 'lint', 'compress'], function () {
     // place code for your default task here
 
 });
 
 gulp.task('lint', function () {
-    return gulp.src('pub/js/**')
+    var eslint = require('gulp-eslint');
+    return gulp.src(config.js)
         .pipe(eslint())
         .pipe(eslint.format())
         // Brick on failure to be super strict
         .pipe(eslint.failOnError());
+});
+
+
+gulp.task('compress', function () {
+    return gulp.src(config.js)
+        .pipe(require('gulp-concat')(config.release.file))
+        .pipe(require('gulp-minify')())
+        .pipe(gulp.dest(config.release.folder))
+});
+
+
+gulp.task("clean", function () {
+    return del([config.release.folder]);
 });
